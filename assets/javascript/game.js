21e76f8a15e7761904c49ccfa669064b0e842b61
leb <leb5313@gmail.com>
2019-02-03 20:58:23 -0500

    added ids to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,45 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var numLeft = 9;
 var guessChoices = [];
-var userGuess = null;
-
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
+var userGuess= null;
 
 document.onkeyup = function(event) {
-  var userGuess = event.key;
+  
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    var options = ["a","b","c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"];
+
+  if (options.indexOf(userGuess) > -1) {
+   
+    if (userGuess == computerGuess) {
+      wins++;
+      numLeft = 9;
+      guessChoices = [];
+
+    } else {
+      
+        if (computerChoices.includes(userGuess)) {
+     
+    } else {
+        numLeft--;
+        guessChoices.push(userGuess);
+      }
+    }
+    
+    if (numLeft === 0) {
+      numLeft = 9;
+      losses++;
+      guessChoices = [];
+    }
+  }
 };
 
-if(options.indexOf(userGuess)>-1) {
-    
-    if(userGuess == computerGuess){
-        wins++;
-        numLeft = 9;
-        guessChoices = [];
-    }
-    else {
-       
-        if(computerChoices.includes(userGuess)){
-        }
-        s
-        else {
-            numLeft --;
-            guessChoices.push(userGuess);
-        }
-
-    }
-    if(numLeft ===0) {
-        numLeft=9;
-        losses ++;
-        guessChoices =[];
-    }
-}
+document.getElementById('won').innerHTML = won;
+document.getElementById('lost').innerHTML = lost;
+document.getElementById('attempts').innerHTML = attempts;
 
